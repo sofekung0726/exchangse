41f6a8abc36220081d499f91149e330dfcd4b287
sofekung0726 <sofekung@gmail.com>
2023-10-24 15:48:29 +0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,47 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"
+import React ,{useState , useEffect}from 'react'
+import { initializeConnector } from '@web3-react/core'
+import { MetaMask } from '@web3-react/metamask'
+import { ethers } from 'ethers'
+import { formatEther,parseUnits } from '@ethersproject/units'
+import Navbar from '@/component/navbar'
+const [metaMask , hooks] = initializeConnector((actions)=> new MetaMask({actions}))
+const {useChainId , useAccounts, useIsActivating,useIsActive,useProvider} = hooks
+const contractChain = 11155111
+const contractAddress = '0x86c6ea21110b777c3af59199880291b602515578'
+const page = () => {
+const chainId = useChainId()
+const accounts = useAccounts()
+const isActive = useIsActive()
+const provider = useProvider()
+const [error, setError] = useState(undefined)
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+useEffect(() => {
+  void metaMask.connectEagerly().catch(() => {
+    console.debug('Failed to connect eagerly to metamask')
+  })
+}, [])
+const handleConnect = () => {
+  metaMask.activate(contractChain)
 }
+
+const handleDisconnect = () => {
+  metaMask.resetState()
+}
+  return (
+    
+    <div>
+      <Navbar accounts = {accounts} chainId = {chainId} isActive = {isActive} provider = {provider}
+       handleConnect={handleConnect} handleDisconnect={handleDisconnect}/>
+      < div className='container'>
+    <div className='card'>
+      <p>chainId: { chainId }</p>
+      <p>isActive: { isActive.toString() }</p>
+      <p>accounts: { accounts ? accounts[0] :''}</p>
+     
+      </div>
+      </div>
+    </div>
+  )}
+
+export default page
---
+++ component/navbar.js
@@ -0,0 +1,67 @@
+"use client"
+import React, { useState, useEffect } from 'react'
+import { initializeConnector } from '@web3-react/core'
+import { MetaMask } from '@web3-react/metamask'
+import { ethers } from 'ethers'
+import { formatEther, parseUnits } from '@ethersproject/units'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import page from '@/app/page';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+const [metaMask, hooks] = initializeConnector((actions) => new MetaMask({ actions }))
+
+const contractChain = 11155111
+const contractAddress = '0x86c6ea21110b777c3af59199880291b602515578'
+ const Navbar= ({handleConnect,handleDisconnect,isActive,accounts,provider}) => {
+    
+    useEffect(() => {
+        void metaMask.connectEagerly().catch(() => {
+            console.debug('Failed to connect eagerly to metamask')
+        })
+    }, [])
+    const [error, setError] = useState(undefined)
+
+    
+    
+  
+    
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        BlockChain 
+                    </Typography>
+
+                    {isActive ?
+                        (<Stack direction="row" spacing={1}>
+                        <Chip label={ accounts} variant="outlined" />
+                            <Button color="inherit" onClick={handleDisconnect}>  Disconnect </Button></Stack>
+                        ) : (
+                            
+                          
+                            <Button color="inherit" onClick={handleConnect}>  Connect </Button>
+                        )
+                    }
+
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+ } 
+export default Navbar;
